b1ee496b2f3a8a674cb7272e3b3a7e16e1ca5479
dmuktesh <muktesh@idyllic-software.com>
2016-10-04 11:59:07 +0530

    [SCHLOOP-1]sign in,sing out functionality for user model is done

--- app/controllers/admin/users/sessions_controller.rb
+++ app/controllers/admin/users/sessions_controller.rb
@@ -1,14 +1,17 @@
 class Admin::Users::SessionsController < Devise::SessionsController
 # before_action :configure_sign_in_params, only: [:create]
+  def after_sign_in_path_for(user)
+    '/admin/users/dashboards/index'
+  end
 
   # GET /resource/sign_in
   # def new
-  #   super
-  # end
+  #   super 
+  #end
 
   # POST /resource/sign_in
   # def create
-  #   super
+  #   super  
   # end
 
   # DELETE /resource/sign_out
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
 
-  devise_for :parents
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'home#index'
 
-  devise_for :users
+  devise_for :users, controllers:{
+                         sessions: 'admin/users/sessions',
+                         registrations: 'admin/users/registrations',
+                         passwords: 'admin/users/passwords',
+                         invitations: 'admin/users/invitations'
+  }
+
 
   devise_for :teachers, controllers:{
                          sessions: 'teachers/sessions',
@@ -15,9 +19,23 @@ Rails.application.routes.draw do
                          passwords: 'teachers/passwords',
                          invitations: 'teachers/invitations'
   }
+
+
+  devise_for :parents, controllers:{
+                         sessions: 'teachers/sessions',
+                         registrations: 'teachers/registrations',
+                         passwords: 'teachers/passwords',
+                         invitations: 'teachers/invitations'
+  }
+
+  namespace :admin do
+    resource :users do
+      get "/dashboards/index" => 'users/dashboards#index'
+    end
+  end
+
   namespace :api do
     namespace :v1 do
-      resources :users
     end
   end
 end
