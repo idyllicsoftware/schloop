96d0e71eb328ce99c37d05ffb81df24cf297aca1
omkar-idyllic <omkar.nazarkar@idyllic-software.com>
2016-10-04 09:39:05 +0530

    [schloop-2]changes

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,2 +1,8 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$(document).ready(function() {
+	$(".sign-up-btn").click(function() {
+		$("sign-up-main-section").scrollTop();
+	});
+});	
