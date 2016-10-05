470c27d19ce809992bbee74f0470c131ede8b5b9
kapil-idyllic <kapil@idyllic-software.com>
2016-10-05 16:40:10 +0530

    [SCHLOOP-1] school code addition

--- app/models/school.rb
+++ app/models/school.rb
@@ -12,16 +12,24 @@
 #  school_director_id :integer
 #  created_at         :datetime         not null
 #  updated_at         :datetime         not null
+#  code               :string           not null
 #
 
 class School < ActiveRecord::Base
   belongs_to :school_director, class_name: 'Teacher'
 
+  before_save :update_school_unique_code
   #TODO::muktesh add field validations
 
 
     private
     def update_school_unique_code
-      
+      counter = (last_school_id + 1).to_s.rjust(4, '0')
+      school_letters = (name.gsub(/\W+/, '').first(2) rescue 'AB')
+      self.code = school_letters.upcase + counter
+    end
+
+    def last_school_id
+      self.last.id rescue 0
     end
 end
