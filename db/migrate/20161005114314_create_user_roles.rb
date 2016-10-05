828c31791a9a6f1f4f42885d9bcb93d190ff2c6b
SGourshete <swapnilggourshete@gmail.com>
2016-10-05 18:09:44 +0530

    [SCHLOOP-12] UserRole model and related code added.

---
+++ db/migrate/20161005114314_create_user_roles.rb
@@ -0,0 +1,11 @@
+class CreateUserRoles < ActiveRecord::Migration
+  def change
+    create_table :user_roles do |t|
+      t.references :user, foreign_key: true
+      t.references :role, foreign_key: true
+      add_index :user_roles, :user_id, :name => 'index_user_roles_on_user_id'
+      add_index :user_roles, [:user_id, :role_id], unique: :true, :name => 'index_user_roles_on_user_id_and_role_id'
+      t.timestamps null: false
+    end
+  end
+end
