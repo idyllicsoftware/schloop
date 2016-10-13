371c4987bc695bdd1804c29c0157cfb31f011e57
omkar-idyllic <omkar.nazarkar@idyllic-software.com>
2016-10-13 16:56:08 +0530

    SCHLOOP-34-schools registration from admin side done

--- db/schema.rb
+++ db/schema.rb
@@ -75,12 +75,12 @@ ActiveRecord::Schema.define(version: 20161009135113) do
   add_index "teachers", ["token"], name: "index_teachers_on_token", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",            null: false
+    t.string   "encrypted_password",     default: "",            null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,             null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
