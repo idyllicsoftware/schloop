b85362ffc86d3e5faaadccb29bac0eb877abdea7
SGourshete <swapnilggourshete@gmail.com>
2016-10-25 18:35:45 +0530

    [SCHLOOP-34]school registration without admin added.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161009135113) do
+ActiveRecord::Schema.define(version: 20161025115120) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20161009135113) do
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
     t.string   "code",               null: false
+    t.string   "board"
   end
 
   create_table "teachers", force: :cascade do |t|
