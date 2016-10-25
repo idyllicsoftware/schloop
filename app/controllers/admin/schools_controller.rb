8cd9c302f3e9f283990aebfc282f7cb8130f355f
SGourshete <swapnilggourshete@gmail.com>
2016-10-25 18:35:45 +0530

    [SCHLOOP-34]school registration without admin added.

--- app/controllers/admin/schools_controller.rb
+++ app/controllers/admin/schools_controller.rb
@@ -3,7 +3,18 @@ class Admin::SchoolsController < ApplicationController
 	end
 
 	def create
-    new_school = School.new(school_params)
+    new_school_admin = SchoolAdmin.new
+    new_school_admin.first_name = params[:administrator_name]
+
+    new_school = School.new
+    new_school.name = params[:name]
+    new_school.website = params[:website]
+    new_school.address = params[:address]
+    new_school.zip_code = params[:zip_code]
+    new_school.phone1 = params[:phone1]
+    new_school.phone2 = params[:phone2]
+    new_school.name = params[:name]
+
     if new_school.save
     	redirect_to admin_school_admins_path
     else
@@ -11,9 +22,16 @@ class Admin::SchoolsController < ApplicationController
     end
 	end
 
+  def edit
+
+    authority_user = User.create #Create new School-Admin
+    school_admin = User.create
+
+  end
+
   private
 
   def school_params
-    params.require(:school).permit(:name, :address, :zip_code, :phone1, :phone2, :website, :code)
+    params.permit(:name, :address, :zip_code, :phone1, :phone2, :website)
   end     
 end
