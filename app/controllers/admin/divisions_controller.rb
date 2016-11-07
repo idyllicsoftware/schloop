2ac3ea0dbf580a4ec4144c117d00927d503197f8
dmuktesh <muktesh@idyllic-software.com>
2016-11-07 19:28:37 +0530

    add division functionality added

--- app/controllers/admin/divisions_controller.rb
+++ app/controllers/admin/divisions_controller.rb
@@ -1,4 +1,9 @@
 class Admin::DivisionsController < ApplicationController
 	def create
-	end
+		division= Division.create(name: params[:div_name], grade_id: params[:grade_id])
+		school = Grade.where(id: params[:grade_id]).pluck(:school_id)
+		redirect_to "/admin/schools/#{school[0]}"
+    end
+    def find_divisions
+    end
 end
