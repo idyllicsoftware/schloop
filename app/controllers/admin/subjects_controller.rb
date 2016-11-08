a3b88dc1486bf099d38111f30c4e0408603030d3
dmuktesh <muktesh@idyllic-software.com>
2016-11-08 11:39:29 +0530

    subject add functionality added

---
+++ app/controllers/admin/subjects_controller.rb
@@ -0,0 +1,14 @@
+class Admin::SubjectsController < ApplicationController
+
+  def create
+  	errors = []
+	begin
+    	subject = Subject.create(name: params[:subject_name],grade_id: params[:grade_id])
+	rescue Exception => e
+		errors << "can't store subject."
+    end
+    render json: {
+    	success: true, errors: errors
+    }
+  end
+end
