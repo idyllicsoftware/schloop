8b135fcdcf8a80668cbc09a943be9cc806ee459b
kapil-idyllic <kapil@idyllic-software.com>
2016-11-15 14:57:02 +0530

    circular enum changes

--- app/models/ecircular.rb
+++ app/models/ecircular.rb
@@ -15,13 +15,13 @@
 
 class Ecircular < ActiveRecord::Base
 	#has_many: attachments
-	has_many :receipients
-	enum circular_type: [ :lesson_plan ,:exam_time_table, :my_result, :my_attendance, :class_timetable, :sample_papers, 
-	 	:transport_details, :holiday_circular, :medical_visit_report,:news_and_events,:important_announcement,:event_circular, 
-	 	:awards_and_achievements,:fee_notice,:follow_up_activity_for_parents,:exhibitions,:worksheets,
-	 	:extra_curricular_activities_circular,:school_time_change,:inter_school_competitions,:intra_school_competitions,:olympiads ]
+	enum circular_type: [:lesson_plan, :exam_time_table, :my_result, :my_attendance, :class_timetable, :sample_papers,
+	 	:transport_details, :holiday_circular, :medical_visit_report, :news_and_events, :important_announcement, :event_circular,
+	 	:awards_and_achievements, :fee_notice, :follow_up_activity_for_parents, :exhibitions, :worksheets,
+	 	:extra_curricular_activities_circular, :school_time_change, :inter_school_competitions, :intra_school_competitions, :olympiads ]
 
  	enum created_by_type: [ :teacher, :school_admin, :product_admin]
 
  	belongs_to :school
+	has_many :receipients
 end
