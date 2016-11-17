1757eec3f1b205950fa1c2402679b003bd14d701
kapil-idyllic <kapil@idyllic-software.com>
2016-11-17 13:02:25 +0530

    typo fix

--- db/schema.rb
+++ db/schema.rb
@@ -102,33 +102,6 @@ ActiveRecord::Schema.define(version: 20161116103706) do
   add_index "parents", ["email"], name: "index_parents_on_email", unique: true, using: :btree
   add_index "parents", ["reset_password_token"], name: "index_parents_on_reset_password_token", unique: true, using: :btree
 
-  create_table "permissions", force: :cascade do |t|
-    t.string   "name"
-    t.string   "controller"
-    t.string   "action"
-    t.text     "flags"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "role_permissions", force: :cascade do |t|
-    t.integer  "role_id"
-    t.integer  "permission_id"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
-  end
-
-  add_index "role_permissions", ["permission_id"], name: "index_role_permissions_on_permission_id", using: :btree
-  add_index "role_permissions", ["role_id"], name: "index_role_permissions_on_role_id", using: :btree
-
-  create_table "roles", force: :cascade do |t|
-    t.string   "name"
-    t.string   "role_map"
-    t.boolean  "status"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "schools", force: :cascade do |t|
     t.string   "name",               null: false
     t.text     "address",            null: false
@@ -142,6 +115,7 @@ ActiveRecord::Schema.define(version: 20161116103706) do
     t.string   "code",               null: false
     t.string   "board"
     t.string   "principal_name"
+    t.string   "logo"
   end
 
   create_table "subjects", force: :cascade do |t|
@@ -150,9 +124,13 @@ ActiveRecord::Schema.define(version: 20161116103706) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
     t.integer  "grade_id"
+    t.integer  "teacher_id"
+    t.integer  "division_id"
   end
 
+  add_index "subjects", ["division_id"], name: "index_subjects_on_division_id", using: :btree
   add_index "subjects", ["grade_id"], name: "index_subjects_on_grade_id", using: :btree
+  add_index "subjects", ["teacher_id"], name: "index_subjects_on_teacher_id", using: :btree
 
   create_table "teachers", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -178,16 +156,6 @@ ActiveRecord::Schema.define(version: 20161116103706) do
   add_index "teachers", ["email"], name: "index_teachers_on_email", unique: true, using: :btree
   add_index "teachers", ["reset_password_token"], name: "index_teachers_on_reset_password_token", unique: true, using: :btree
   add_index "teachers", ["token"], name: "index_teachers_on_token", using: :btree
-
-  create_table "user_roles", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "role_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_index "user_roles", ["user_id", "role_id"], name: "index_user_roles_on_user_id_and_role_id", unique: true, using: :btree
-  add_index "user_roles", ["user_id"], name: "index_user_roles_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "",            null: false
@@ -223,9 +191,7 @@ ActiveRecord::Schema.define(version: 20161116103706) do
   add_foreign_key "grade_teachers", "subjects"
   add_foreign_key "grade_teachers", "teachers"
   add_foreign_key "grades", "schools"
-  add_foreign_key "role_permissions", "permissions"
-  add_foreign_key "role_permissions", "roles"
+  add_foreign_key "subjects", "divisions"
   add_foreign_key "subjects", "grades"
-  add_foreign_key "user_roles", "roles"
-  add_foreign_key "user_roles", "users"
+  add_foreign_key "subjects", "teachers"
 end
