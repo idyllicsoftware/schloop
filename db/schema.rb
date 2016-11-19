d81e7de966772ad45b3d4e223cd1b2bb972aafcf
dmuktesh <muktesh@idyllic-software.com>
2016-11-19 11:01:18 +0530

    schema change

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161117113620) do
+ActiveRecord::Schema.define(version: 20161118100242) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -101,17 +101,43 @@ ActiveRecord::Schema.define(version: 20161117113620) do
   add_index "parents", ["email"], name: "index_parents_on_email", unique: true, using: :btree
   add_index "parents", ["reset_password_token"], name: "index_parents_on_reset_password_token", unique: true, using: :btree
 
+  create_table "permissions", force: :cascade do |t|
+    t.string   "name"
+    t.string   "controller"
+    t.string   "action"
+    t.text     "flags"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "role_permissions", force: :cascade do |t|
+    t.integer  "role_id"
+    t.integer  "permission_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
+  add_index "role_permissions", ["permission_id"], name: "index_role_permissions_on_permission_id", using: :btree
+  add_index "role_permissions", ["role_id"], name: "index_role_permissions_on_role_id", using: :btree
+
+  create_table "roles", force: :cascade do |t|
+    t.string   "name"
+    t.string   "role_map"
+    t.boolean  "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "schools", force: :cascade do |t|
-    t.string   "name",               null: false
-    t.text     "address",            null: false
-    t.string   "zip_code",           null: false
-    t.string   "phone1",             null: false
+    t.string   "name",           null: false
+    t.text     "address",        null: false
+    t.string   "zip_code",       null: false
+    t.string   "phone1",         null: false
     t.string   "phone2"
-    t.string   "website",            null: false
-    t.integer  "school_director_id"
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
-    t.string   "code",               null: false
+    t.string   "website",        null: false
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+    t.string   "code",           null: false
     t.string   "board"
     t.string   "principal_name"
     t.string   "logo"
@@ -156,6 +182,16 @@ ActiveRecord::Schema.define(version: 20161117113620) do
   add_index "teachers", ["reset_password_token"], name: "index_teachers_on_reset_password_token", unique: true, using: :btree
   add_index "teachers", ["token"], name: "index_teachers_on_token", using: :btree
 
+  create_table "user_roles", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "role_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "user_roles", ["user_id", "role_id"], name: "index_user_roles_on_user_id_and_role_id", unique: true, using: :btree
+  add_index "user_roles", ["user_id"], name: "index_user_roles_on_user_id", using: :btree
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "",            null: false
     t.string   "encrypted_password",     default: "",            null: false
@@ -190,7 +226,11 @@ ActiveRecord::Schema.define(version: 20161117113620) do
   add_foreign_key "grade_teachers", "subjects"
   add_foreign_key "grade_teachers", "teachers"
   add_foreign_key "grades", "schools"
+  add_foreign_key "role_permissions", "permissions"
+  add_foreign_key "role_permissions", "roles"
   add_foreign_key "subjects", "divisions"
   add_foreign_key "subjects", "grades"
   add_foreign_key "subjects", "teachers"
+  add_foreign_key "user_roles", "roles"
+  add_foreign_key "user_roles", "users"
 end
