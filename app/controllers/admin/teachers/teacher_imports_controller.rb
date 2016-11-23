e20fc5298d6f2d992ba41ae71b7092f43fc3bd6a
Chetan Thakkar <chetan@idyllic-software.com>
2016-11-23 11:43:55 +0530

    resolved conflicts

---
+++ app/controllers/admin/teachers/teacher_imports_controller.rb
@@ -0,0 +1,17 @@
+class Admin::Teachers::TeacherImportsController < ApplicationController
+	before_action :authenticate_user!
+  layout "admin"
+	def new
+    @teacher_import = TeacherImport.new
+  end
+
+  def create
+    @school = School.find(params[:school_id])
+    @teacher_import = TeacherImport.new(params[:teacher_import], params[:school_id])
+    if @teacher_import.save
+      redirect_to :back, notice: "Imported teachers successfully."
+    else
+      redirect_to :back, errors: "Please check csv data."
+    end
+  end
+end
