c1821dfc5eecf040cd8bb16ce519fa5b545c6aa5
Radheshyam Nazarkar <radheshyam.nazarkar@gmail.com>
2016-11-25 16:15:36 +0530

    file upload files added

--- app/controllers/admin/parent_imports_controller.rb
+++ app/controllers/admin/parent_imports_controller.rb
@@ -1,15 +1,19 @@
 class Admin::ParentImportsController < ApplicationController
   before_action :authenticate_user!
+  before_action :find_school, only: [:new, :create]
   layout "admin"
+
   def new
-  	@school = School.find(params[:school_id])
-    @grade = Grade.find(params[:grade_id])
+    redirect_to '/' and return if @school.blank?
+    @grade = @school.grades.find(params[:grade_id])
+    redirect_to admin_school_path(@school.id) and return if @grade.blank?
     @parent_import = ParentImport.new({}, @school.id, @grade.id)
   end
 
   def create
-    @school = School.find(params[:school_id])
-    @grade = Grade.find(params[:grade_id])
+    redirect_to '/' and return if @school.blank?
+    @grade = @school.grades.find(params[:grade_id])
+    redirect_to admin_school_path(@school.id) and return if @grade.blank?
     @parent_import = ParentImport.new(params[:parent_import], params[:school_id], params[:grade_id])
     if @parent_import.save
       redirect_to admin_school_path(@school.id)
@@ -17,4 +21,10 @@ class Admin::ParentImportsController < ApplicationController
       render :action => "new" #,:school_id => @school.id, :grade_id => @grade 
     end
   end
+
+  private
+  def find_school
+    @school = School.find_by(id: params[:school_id])
+  end
+
 end
