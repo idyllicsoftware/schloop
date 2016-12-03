907adfa3f81ab052a03ee7482bc23c842f32d68e
dmuktesh <muktesh@idyllic-software.com>
2016-12-03 15:51:29 +0530

    file attachment functionality added for ecircular

--- config/routes.rb
+++ config/routes.rb
@@ -47,6 +47,7 @@ Rails.application.routes.draw do
       resources :ecirculars, only: [:create], shallow: true do
         collection do
           get :all
+          post :upload_file
         end
       end
 
