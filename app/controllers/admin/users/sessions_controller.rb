4eafe1ff54539edc47ad00a46271546705728a24
dmuktesh <muktesh@idyllic-software.com>
2016-12-07 11:28:15 +0530

    user sign in failed response added

--- app/controllers/admin/users/sessions_controller.rb
+++ app/controllers/admin/users/sessions_controller.rb
@@ -1,5 +1,6 @@
 class Admin::Users::SessionsController < Devise::SessionsController
 # before_action :configure_sign_in_params, only: [:create]
+  respond_to :json
   def after_sign_in_path_for(user)
     if user.type == "ProductAdmin"
       '/admin/schools'
@@ -17,9 +18,13 @@ class Admin::Users::SessionsController < Devise::SessionsController
   # end
 
   # POST /resource/sign_in
-  # def create
-  #   super  
-  # end
+  def create
+    if user_signed_in? 
+      super
+    else
+      render json: { errors: "Invalid email or password"} and return  
+    end
+  end
 
   # DELETE /resource/sign_out
   # def destroy
