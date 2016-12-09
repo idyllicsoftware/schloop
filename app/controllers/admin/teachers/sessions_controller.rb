d8c6935d0bb643c5a9e4b13d0c9a90716265d2eb
dmuktesh <muktesh@idyllic-software.com>
2016-12-09 14:12:45 +0530

    topics data creaeted using seed

--- app/controllers/admin/teachers/sessions_controller.rb
+++ app/controllers/admin/teachers/sessions_controller.rb
@@ -2,9 +2,4 @@ class Admin::Teachers::SessionsController < Devise::SessionsController
   def after_sign_in_path_for(teacher)
     '/admin/topics'
   end
-  
-  def create
-    binding.pry 
-    super 
-  end
 end
--- app/controllers/admin/topics_controller.rb
+++ app/controllers/admin/topics_controller.rb
@@ -2,9 +2,27 @@ class Admin::TopicsController < ApplicationController
 
   def index
     teacher = current_teacher
+    grade_id = Teacher.last.grade_teachers.minimum(:grade_id)
+    subject_id = Teacher.last.grade_teachers.minimum(:subject_id)
+    topics = Topic.where(grade_id: grade_id, teacher_id: teacher.id, subject_id: subject_id)
+    grade_teachers_data_service = GradeTeachersDataService.new
+    grade_teachers_data = grade_teachers_data_service.get_grade_teacher_data(teacher.id) 
+
+    if topics.blank?
+      render json: { msg: "no record found" }
+    else
+      render json: {topics: topics, grade_teachers_data: grade_teachers_data}
+    end
+  end
+
+  def show
+    teacher = current_teacher
     school = teacher.school
     grade_teachers_data_service = GradeTeachersDataService.new
     grade_teachers_data = grade_teachers_data_service.get_grade_teacher_data(teacher.id) 
     render json: { success: true, data: grade_teachers_data }
   end
+
+  def get_topics(teacher_id,grade_id,subject_id)
+  end 
 end
