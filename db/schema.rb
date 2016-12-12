fcdb200eb37cb77d62150518c85713c5516a8411
dmuktesh <muktesh@idyllic-software.com>
2016-12-12 11:47:56 +0530

    schema change

--- db/schema.rb
+++ db/schema.rb
@@ -156,21 +156,21 @@ ActiveRecord::Schema.define(version: 20161210105918) do
   end
 
   create_table "parents", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  limit: 100, default: "", null: false
+    t.string   "encrypted_password",                 default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",                      default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.text     "first_name",                          null: false
-    t.text     "last_name",                           null: false
-    t.text     "guardian_type",                       null: false
+    t.datetime "created_at",                                      null: false
+    t.datetime "updated_at",                                      null: false
+    t.text     "first_name",                                      null: false
+    t.text     "last_name",                                       null: false
+    t.text     "guardian_type",                                   null: false
   end
 
   add_index "parents", ["email"], name: "index_parents_on_email", unique: true, using: :btree
@@ -243,14 +243,10 @@ ActiveRecord::Schema.define(version: 20161210105918) do
     t.datetime "created_at",                    null: false
     t.datetime "updated_at",                    null: false
     t.integer  "grade_id"
-    t.integer  "teacher_id"
-    t.integer  "division_id"
     t.integer  "master_subject_id", default: 0, null: false
   end
 
-  add_index "subjects", ["division_id"], name: "index_subjects_on_division_id", using: :btree
   add_index "subjects", ["grade_id"], name: "index_subjects_on_grade_id", using: :btree
-  add_index "subjects", ["teacher_id"], name: "index_subjects_on_teacher_id", using: :btree
 
   create_table "teachers", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -292,6 +288,20 @@ ActiveRecord::Schema.define(version: 20161210105918) do
   add_index "teachers", ["invited_by_id"], name: "index_teachers_on_invited_by_id", using: :btree
   add_index "teachers", ["reset_password_token"], name: "index_teachers_on_reset_password_token", unique: true, using: :btree
   add_index "teachers", ["token"], name: "index_teachers_on_token", using: :btree
+
+  create_table "topics", force: :cascade do |t|
+    t.string   "title"
+    t.integer  "grade_id"
+    t.integer  "subject_id"
+    t.integer  "teacher_id"
+    t.boolean  "is_master"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "topics", ["grade_id"], name: "index_topics_on_grade_id", using: :btree
+  add_index "topics", ["subject_id"], name: "index_topics_on_subject_id", using: :btree
+  add_index "topics", ["teacher_id"], name: "index_topics_on_teacher_id", using: :btree
 
   create_table "user_roles", force: :cascade do |t|
     t.integer  "role_id"
@@ -348,8 +358,9 @@ ActiveRecord::Schema.define(version: 20161210105918) do
   add_foreign_key "grades", "schools"
   add_foreign_key "role_permissions", "permissions"
   add_foreign_key "role_permissions", "roles"
-  add_foreign_key "subjects", "divisions"
   add_foreign_key "subjects", "grades"
-  add_foreign_key "subjects", "teachers"
+  add_foreign_key "topics", "grades"
+  add_foreign_key "topics", "subjects"
+  add_foreign_key "topics", "teachers"
   add_foreign_key "user_roles", "roles"
 end
