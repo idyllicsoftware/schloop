4d3b8e1112dc1b7aeb1fb080db29f103ac308e30
dmuktesh <muktesh@idyllic-software.com>
2016-12-13 17:33:00 +0530

    redirection of teacher after login

--- app/controllers/admin/teachers/sessions_controller.rb
+++ app/controllers/admin/teachers/sessions_controller.rb
@@ -1,6 +1,7 @@
 class Admin::Teachers::SessionsController < Devise::SessionsController
- 
+
   def after_sign_in_path_for(teacher)
-    "/admin/teachers/dashboards/#{teacher.id}"
+    redirect_to "/admin/teachers/dashboards/#{teacher.id}"
   end
+  
 end
