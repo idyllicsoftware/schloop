9b41eedc7692f6981a174ac835b9e645db322b44
dmuktesh <muktesh@idyllic-software.com>
2016-12-14 17:14:12 +0530

    create bookmark method added

--- app/controllers/admin/teachers/dashboards_controller.rb
+++ app/controllers/admin/teachers/dashboards_controller.rb
@@ -2,11 +2,25 @@ class Admin::Teachers::DashboardsController < ApplicationController
 
 	layout "teacher"
 	
-  def show
-    subject_id = Subject.select('master_subject_id').where(id: params[:subject_id] )
-    grade_id = Grade.select('master_grade_id').where(id: params[:grade_id] )
-
-    @topics = Topic.where(teacher_id: [params[:id],-1]).where(master_grade_id: grade_id).where(masater_subject_id: subject_id)
-    @bookmarsk = Bookmark.where(teacher_id: params[:id]).where(grade_id: grade_id.master_grade_id).where(subject_id: subject_id.master_subject_id)
+  def create
+    errors = []
+    begin
+      teacher = Teacher.find_by(id: params[:id])
+      bookmark_data = {
+        title: params[:title],
+        subject_id: params[:subject_id],
+        grade_id:  params[:grade_id],
+        topic_id:  params[:topic_id],
+        data:  params[:data],
+        data_type: params[:data_type],
+        caption:  params[:caption],
+        teacher_id:  Teacher.find_by(id: params[:id]).id,
+        school_id:  School.find_by(id: teacher.school_id).id
+       }
+       Bookmark.create(bookmark_data)
+    rescue Exception => e
+      errors << "error while creating new bookmark"
+    end
+    render json: {success: errors.blank?, errors: errors}
   end
 end
