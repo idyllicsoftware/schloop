6da2024123378d77cda2b5c86b316a678f3d79c4
kapil-idyllic <kapil@idyllic-software.com>
2017-01-10 18:36:46 +0530

    ip addresses changed to domain names

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,7 +1,7 @@
 branch = (ENV['branch'].nil? ? %x(git symbolic-ref --short -q HEAD).delete("\n") : ENV['branch'])
 branch = "develop" if branch == ""
 
-set :domain, '52.23.232.204'
+set :domain, 'staging.schloop.co'
 set :branch, branch
 set :user, 'ubuntu'
 set :rails_env,'staging'
