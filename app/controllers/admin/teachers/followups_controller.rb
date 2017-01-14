a38827ad69cb041ee38c9bb0f53556a19f7c7f8b
dmuktesh <muktesh@idyllic-software.com>
2017-01-14 16:19:28 +0530

    create and index action added for followup controller in web

--- app/controllers/admin/teachers/followups_controller.rb
+++ app/controllers/admin/teachers/followups_controller.rb
@@ -1,18 +1,25 @@
 class Admin::Teachers::FollowupsController < ApplicationController
 
   def create
-    errors, data = [], {}
+    errors = []
     begin
       bookmark = Bookmark.find_by(id: followup_params[:bookmark_id])
-      grade = bookmark.grade
-      student_ids = StudentProfile.where(grade_id: grade.id).where(status: 'active').pluck(:student_id)
-      parent_ids = Student.pluck(:parent_id).where(id: student_ids)
-      first_parent = Parent.find_by(id: parent_ids.first)
-      data = {parent_name: first_parent.name, count: parent_ids.count}
+      errors << "Please provide valid bookmark." if bookmark.nil?
+      errors << "Bookmark already sent to followup." if bookmark.followup.present?
+      if errors.blank?
+        followup = Followup.create(bookmark: bookmark)
+        errors << followup.errors.full_messages if followup.errors.present?
+      end
     rescue Exception => e
       errors << "error occured while sharing schloopmark with parents" + ', ' + e.message
     end
-    render json: {success: errors.blank?, errors: errors,  data: data}
+    render json: {success: errors.blank?, errors: errors}
+  end
+
+  def index
+    teacher = current_teacher
+    followup_datum, no_of_records = Followup.index(teacher)
+    render json: {success: true, data: followup_datum}
   end
 
   private
