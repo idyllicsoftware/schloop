7244394b280fac6c9b53ac612478bf4793f9524f
dmuktesh <muktesh@idyllic-software.com>
2017-01-31 16:48:45 +0530

    migration for user,roles, permission added

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170131053827) do
+ActiveRecord::Schema.define(version: 20170131110216) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
