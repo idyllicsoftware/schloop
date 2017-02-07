bd0251b642d97ee6f96f3de8a41e75a4693c3276
kapil-idyllic <kapil@idyllic-software.com>
2017-02-07 16:38:27 +0530

    File upload service, name pattern bug fix.

--- app/services/file_upload_service.rb
+++ app/services/file_upload_service.rb
@@ -6,11 +6,12 @@ class FileUploadService < BaseService
       ext = File.extname(input_file.original_filename)
       file_name = File.basename(input_file.original_filename, '.*')
       file_name = "#{file_name}_#{Time.now.to_i}#{ext}"
+      encoded_file_name = "#{file_name.hash}#{ext}"
       file_params = {
         original_filename: file_name,
         file_size: input_file.size,
         sub_type: options[:sub_type],
-        url: "public/upload/#{Time.now.to_i}_#{file_name}",
+        url: "public/upload/#{Time.now.to_i}_#{encoded_file_name}",
       }
       attachment = Attachment.upload_file(input_file, file_params, { }, { acl: 'public-read', original_filename: file_params[:original_filename] })
       attachment.attachable = source
