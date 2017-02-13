3ec6feabf5abbb5e4725cbed83e0d9c3cd64f9c3
kapil-idyllic <kapil@idyllic-software.com>
2017-02-13 12:54:43 +0530

    route added

--- config/routes.rb
+++ config/routes.rb
@@ -215,6 +215,7 @@ Rails.application.routes.draw do
       get  "/followups/:bookmark_id/view" => "followups#view"
       post "/followups/:bookmark_id/comment" => 'followups#comment'
 
+      get  "/followups/activities" => "activities#shared_activities"
 
     end
   end
